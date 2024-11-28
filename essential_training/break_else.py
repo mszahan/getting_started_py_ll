5e2332c51974f00b79985edd71d519f87626ee20
mszahan <mszahan1@gmail.com>
2024-11-28 15:08:29 +0600

    explored break else in loops

---
+++ essential_training/break_else.py
@@ -0,0 +1,9 @@
+#checking break else statements
+
+for number in range(2, 100):
+    for factor in range(2, int(number**0.5)+1):
+        if number % factor ==0:
+            break
+
+    else:
+        print(f'{number} is prime')
