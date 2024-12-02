e083cde7b1e276cbfb97718f74d26e612f41b3c3
mszahan <mszahan1@gmail.com>
2024-12-02 13:21:48 +0600

    explored the multiple inheritance

---
+++ oop_py/ch2/multiple_inheritance.py
@@ -0,0 +1,29 @@
+
+class A:
+    def __init__(self):
+        super().__init__()
+        self.prop1 = 'prop1'
+        self.name = 'Class A'
+    
+
+class B:
+    def __init__(self):
+        super().__init__()
+        self.prop2 = 'prop2'
+        self.name = 'Class B'
+
+
+class C(A, B):
+    def __init__(self):
+        super().__init__()
+
+    def showprops(self):
+        print(self.prop1)
+        print(self.prop2)
+        ## this name will come from the first class it's inherit (if found)
+        print(self.name)
+
+
+c = C()
+print(C.__mro__)
+c.showprops()
