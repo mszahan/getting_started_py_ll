ca1a49038cc3160de4ba46696a79a1cb346bfd19
mszahan <mszahan1@gmail.com>
2024-12-07 22:05:35 +0600

    explored some of the methods of math library

---
+++ library_py/ch02_math/01_trigono_ceil_floor_constant.py
@@ -0,0 +1,27 @@
+import math
+
+##constants-------------------
+
+print(math.pi)
+print(math.e)
+
+# print(math.nan)
+# print(math.inf)
+# print(-math.inf)
+
+## trigonomitry-----------------
+
+## cos 45 degree
+obst_direction = math.cos(math.pi/4)
+print(obst_direction)
+print(math.sin(math.pi/4))
+
+
+## ceiling and floor
+cookies = 10.3
+candy = 7
+print(math.ceil(cookies)) # it will pritn 11
+print(math.ceil(candy)) # it will print 7
+
+age = 28.9
+print(math.floor(age)) # it will print 28
---
+++ library_py/ch02_math/02_facto_sroot_gcd.py
@@ -0,0 +1,17 @@
+import math
+
+## Factorial and Square root
+print(math.factorial(3))
+print(math.sqrt(64))
+
+## greatest common denominator GCD
+print(math.gcd(52, 8)) # gosagu
+print(math.gcd(24, 36, 82))
+
+
+## Degrees and radians
+## degree to radian
+print(math.radians(360)) # 2pi radin = 360 degree
+print(math.pi * 2)
+
+print(math.degrees(math.pi * 2)) # it will give 360 degree back
