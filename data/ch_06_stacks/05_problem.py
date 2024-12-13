2c0e11cdb0431d3efaaeaa7ca4d081d5c64a9931
mszahan <mszahan1@gmail.com>
2024-12-13 21:19:59 +0600

    solved paretheses checking with stack

---
+++ data/ch_06_stacks/05_problem.py
@@ -0,0 +1,18 @@
+from collections import deque
+
+def check_matching_parentheses(s):
+    stack = deque()
+    for char in s:
+        if char == '(':
+            stack.append(char)
+        elif char == ')':
+            if not stack:
+                return False
+            stack.pop()
+    return len(stack) == 0
+
+
+print(check_matching_parentheses('()'))
+print(check_matching_parentheses('(hi there)'))
+
+print(check_matching_parentheses('(hi(therer)'))
