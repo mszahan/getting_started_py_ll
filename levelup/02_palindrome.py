86fce3e18e223b80c9a5f7a07aa9691e931bcefc
mszahan <mszahan1@gmail.com>
2024-12-13 22:09:06 +0600

    solved the palindrom problem

---
+++ levelup/02_palindrome.py
@@ -0,0 +1,8 @@
+import re
+
+def is_palindrome(phrase):
+    forwards = ''.join(re.findall(r'[a-z]+', phrase.lower()))
+    backwards = forwards[::-1]
+    return forwards == backwards
+
+print(is_palindrome('madam'))
